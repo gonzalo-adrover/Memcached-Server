0b8e2101f951bb08f3be4c89c592968bf2bf735c
Gonzalo <adrovergonzalo@gmail.com>
2021-03-02 01:15:17 -0300

    Removed unused files and finished command_checker_retrieval
    The checker is also set to work on all retrieval methods.

--- main/test.rb
+++ main/test.rb
@@ -5,19 +5,18 @@ class Test
       attr_accessor :data_hash
 
       def initialize
-      @data_hash = Hash.new
+            @data_hash = Hash.new
       end
 
-      data_hash = {:foo => 0, :bar => 1, :baz => 2}
-
-      line = gets
-
-
-      def getr(line)
-            puts data_hash[:line]
+      def storex(line)
+            now = Time.now
+            time = now + line
+            data_hash.store('Time',time)
       end
       
-      
+      x = gets
+      self.storex(x)
+      puts(data_hash)
       #my_hash = {}
       #my_hash[:my_key] = 'value'
       #puts my_hash.has_key?("my_key".to_sym)
